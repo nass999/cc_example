898ea0ee02ff57fd3945898c7046d8578c62f226
Jacob Lifshay <programmerjake@gmail.com>
2015-06-16 09:05:22 -0600

    add script documentation and remove sudo for apt-get so we don't have problems with no sudo access

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the word count
-
 # first I'll load all my dependencies
-sudo apt-get install g++-4.8
+apt-get install g++-4.8
 
+# then I get a list of input files
 mapfile -t files < <(printf "%s\n" wc_input/*)
 
+# then I make the compiler output directory
 mkdir -p bin/Release
+
+# then I make the output file directory
+mkdir -p wc_output
+
+# then I build and run it
 g++-4.8 -std=c++11 -Wall -O2 -o bin/Release/cc-example main.cpp && bin/Release/cc-example "${files[@]}"
 
 
